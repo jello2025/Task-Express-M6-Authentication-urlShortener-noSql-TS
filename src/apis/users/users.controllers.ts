abca03fd2ee416b79f2e3ce553f58a2dfa349f9a
jello2025 <albarjas2025@gmail.com>
2025-09-02 21:20:53 +0300

    sign in kinda done

--- src/apis/users/users.controllers.ts
+++ src/apis/users/users.controllers.ts
@@ -3,7 +3,6 @@ import bcrypt from "bcrypt";
 import User from "../../models/User";
 import jwt from "jsonwebtoken";
 import { env } from "../../config/env";
-import dotenv from "dotenv";
 export const signup = async (
   req: Request,
   res: Response,
@@ -15,11 +14,15 @@ export const signup = async (
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     const newUser = await User.create({ username, password: hashedPassword });
 
-    const payload = { userId: newUser._id };
+    const payload = { userId: newUser._id, username: username };
     const secret = env.JWT_SECRET!;
     const options = { expiresIn: env.JWT_EXP } as jwt.SignOptions;
     const token = jwt.sign(payload, secret, options);
-    res.status(201).json({ token });
+    res.status(201).json({
+      username: username,
+      password: hashedPassword,
+      token: token,
+    });
   } catch (err) {
     next(err);
   }
@@ -31,6 +34,40 @@ export const signin = async (
   next: NextFunction
 ) => {
   try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return next({
+        status: 400,
+        message: "invalid credentials 1",
+      });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return next({
+        status: 400,
+        message: "invalid credentials 2",
+      });
+    }
+
+    console.log(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password as string);
+
+    if (!isMatch) {
+      return next({
+        status: 400,
+        message: "invalid credentials 3",
+      });
+    }
+    const payload = { userId: user._id, username: username };
+    const secret = env.JWT_SECRET!;
+    const options = { expiresIn: env.JWT_EXP } as jwt.SignOptions;
+    const token = jwt.sign(payload, secret, options);
+    res.status(200).json({
+      token: token,
+    });
   } catch (err) {
     next(err);
   }
--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -1,8 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 
-const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).json({ message: "Internal Server Error" });
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(500).json({ message: "Internal Server Error" });
 };
 
 export default errorHandler;
