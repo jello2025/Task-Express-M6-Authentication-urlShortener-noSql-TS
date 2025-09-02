e4f9c54bff7992b8726e1d9b8b4e1e6083745edb
jello2025 <albarjas2025@gmail.com>
2025-09-02 18:46:09 +0300

    set up done

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,20 @@ import errorHandler from "./middlewares/ErrorHandler";
 import morgan from "morgan";
 import usersRouter from "./apis/users/users.routes";
 import urlsRouter from "./apis/urls/urls.routes";
-
+import dotenv from "dotenv";
+import { env } from "./config/env";
 const app = express();
-const PORT = 8000;
 
 app.use(express.json());
 app.use(morgan("dev"));
+dotenv.config();
+
+const PORT = env.PORT || "5000";
+const DB_URL = env.DB_URL;
+
+if (!DB_URL) {
+  throw new Error("DB_URL is not defined in environment variables");
+}
 
 app.use("/api/users", usersRouter);
 app.use("/api/urls", urlsRouter);
@@ -20,5 +28,5 @@ app.use(errorHandler);
 
 connectDB();
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
