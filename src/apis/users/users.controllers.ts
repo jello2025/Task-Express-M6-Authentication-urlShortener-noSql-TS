6363089bd47aed85c1689be1717c444a7292669a
jello2025 <albarjas2025@gmail.com>
2025-09-02 19:10:51 +0300

    done sign up

--- src/apis/users/users.controllers.ts
+++ src/apis/users/users.controllers.ts
@@ -1,27 +1,50 @@
 import { NextFunction, Request, Response } from "express";
+import bcrypt from "bcrypt";
 import User from "../../models/User";
+import jwt from "jsonwebtoken";
+import { env } from "../../config/env";
+import dotenv from "dotenv";
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { username, password } = req.body;
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = await User.create({ username, password: hashedPassword });
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const newUser = await User.create(req.body);
-        res.status(201).json(newUser);
-    } catch (err) {
-        next(err);
-    }
+    const payload = { userId: newUser._id };
+    const secret = env.JWT_SECRET!;
+    const options = { expiresIn: env.JWT_EXP } as jwt.SignOptions;
+    const token = jwt.sign(payload, secret, options);
+    res.status(201).json({ token });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const signin = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-    } catch (err) {
-        next(err);
-    }
+export const signin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const users = await User.find().populate("urls");
-        res.status(201).json(users);
-    } catch (err) {
-        next(err);
-    }
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find().populate("urls");
+    res.status(201).json(users);
+  } catch (err) {
+    next(err);
+  }
 };
--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { env } from "./config/env";
 
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || "");
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(env.DB_URL || "");
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
